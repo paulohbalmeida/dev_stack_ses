df11ac65a9eaa600557484e737a78402209c7d87
paulohbalmeida <paulohbalmeida@gmail.com>
2018-09-20 17:40:06 -0300

    path sesplan_branch removido

--- sistema/include/config.php
+++ sistema/include/config.php
@@ -7,8 +7,8 @@ $url = "http://" . $dominio;
 $_SESSION["txt_host"] = $url;
 $_SESSION["url_api_mgi"] = "";
 
-$_SESSION["txt_pagina_login"] = $_SESSION["txt_host"]."/sesplan_branch/sesplan/index.html";
-$_SESSION["txt_caminho_aplicacao"] = $_SESSION["txt_host"]."/sesplan_branch/sesplan/sistema";
+$_SESSION["txt_pagina_login"] = $_SESSION["txt_host"]."/sesplan/index.html";
+$_SESSION["txt_caminho_aplicacao"] = $_SESSION["txt_host"]."/sesplan/sistema";
 $_SESSION["txt_sigla_sistema"] = "SESPLAN";
 $_SESSION["txt_pagina_inicial"] = $_SESSION["txt_caminho_aplicacao"]."/index.php";
 $_SESSION["cod_auditoria"] = "0";
